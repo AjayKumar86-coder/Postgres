26609b32afd3c46cfd85e619a870f8f2b9c78749
pajay <pajay@LAP061.stratapps.com>
2020-01-13 09:20:21 +0530

    First Commit

---
+++ SpringBootGoogleLogin/src/main/java/com/springboot/HomeController.java
@@ -0,0 +1,24 @@
+package com.springboot;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController 
+{
+	@RequestMapping("/login")
+	public String login() 
+	{
+		return "Hello User";
+	}
+	
+	@RequestMapping("user")
+	@ResponseBody
+	public Principal user(Principal principal) 
+	{
+		return principal;
+	}
+}
